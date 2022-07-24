38025c6bbfe39262ac39b6c51bef07bb1c752bbf
Anwar Hossain <mdanwarhossain1996bd@gmail.com>
2022-07-24 21:59:11 +0600

    done

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,27 +5,49 @@ const ApiFeatures = require("../utils/apifeatures");
 
 //Create Product
 
-exports.createProduct = catchAsyncErrors( async(req,res,next)=>{
-    
+exports.createProduct = catchAsyncErrors( async(req,res,next)=>{ 
    const product= await Product.create(req.body)
-
     res.status(201).json({
         success:true,
         product,
     })
 })
 
-
-exports.getAllProducts =  catchAsyncErrors(  async(req,res,next)=>{
-
-    const resultPerPage =20 ;
-
-    const apiFeature = new ApiFeatures(Product.find(),req.query).filter().pagination(resultPerPage)
-    
-    const products = await apiFeature.query
-
+exports.ProductmodelSummary =  catchAsyncErrors(  async(req,res,next)=>{   
+    const products = await Product.find().sort({createdAt:-1}) ;
     res.status(200).json({
         success:true,
         products
     })
 })
+
+
+exports.getAllProducts =  catchAsyncErrors(  async(req,res,next)=>{
+    const resultPerPage =20 ;
+    const apiFeature = new ApiFeatures(Product.find(),req.query).search().filter().pagination(resultPerPage)
+    const products = await apiFeature.query
+    res.status(200).json({
+        success:true,
+        products
+    })
+})
+
+
+// single products 
+// exports.getSingleProduct = catchAsyncErrors(  async(req,res,next)=>{
+
+//     const product = await Product.findById(req.params.id)
+
+//     if(!product){
+//         return res.status(500).json({
+//             success:false,
+//             message:"Product not found"
+//         })
+//     }
+
+//     res.status(200).json({
+//         success:true,
+//         product
+//     })
+// })
+
--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,10 +1,12 @@
 const express =require ("express");
-const { getAllProducts , createProduct} = require("../controllers/productController");
+const { getAllProducts , createProduct,ProductmodelSummary,getSingleProduct} = require("../controllers/productController");
 
 
 const router =express.Router();
 
 router.route("/products").get(getAllProducts);
+router.route("/products/summary").get(ProductmodelSummary);
+// router.route("/products/single").get(getSingleProduct);
 router.route("/product/new").post(createProduct)
 
 
